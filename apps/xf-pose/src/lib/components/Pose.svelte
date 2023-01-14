8f33e3de6c97cf4750c43155418cbf80e44a739a
Fredrik Holsten <frh@mindfuture.com>
2023-01-14 12:27:13 +0100

    addded limited head and torso movement

--- apps/xf-pose/src/lib/components/Pose.svelte
+++ apps/xf-pose/src/lib/components/Pose.svelte
@@ -19,43 +19,55 @@
   let poseResults: Results;
   let lookat_pos: Vector3 | undefined;
 
-  const geometry = new SphereGeometry(0.1, 32, 16);
+  const geometry = new SphereGeometry(0.05, 32, 16);
   const material = new MeshBasicMaterial({ color: 0xffff00 });
 
   let node_pos;
   let child_pos;
   let mesh;
-  let mesh2;
+
   let target = new Vector3();
+  let visibility;
 
   let skeleton: {
-    node_id: number;
-    child_id: number;
+    node_ids: number[];
+    child_ids: number[];
     bone: Object3D<Event> | undefined;
+    excludeXrotation: boolean;
   }[];
 
   $: {
     if (nodes) {
       skeleton = [
         {
-          node_id: 11,
-          child_id: 13,
+          node_ids: [11],
+          child_ids: [13],
           bone: nodes.Scene.getObjectByName("mixamorigRightArm"),
+          excludeXrotation: false,
         },
         {
-          node_id: 13,
-          child_id: 15,
+          node_ids: [13],
+          child_ids: [15],
           bone: nodes.Scene.getObjectByName("mixamorigRightForeArm"),
+          excludeXrotation: true,
         },
         {
-          node_id: 12,
-          child_id: 14,
+          node_ids: [12],
+          child_ids: [14],
           bone: nodes.Scene.getObjectByName("mixamorigLeftArm"),
+          excludeXrotation: false,
         },
         {
-          node_id: 14,
-          child_id: 16,
+          node_ids: [14],
+          child_ids: [16],
           bone: nodes.Scene.getObjectByName("mixamorigLeftForeArm"),
+          excludeXrotation: true,
+        },
+        {
+          node_ids: [23, 24],
+          child_ids: [11, 12],
+          bone: nodes.Scene.getObjectByName("mixamorigSpine"),
+          excludeXrotation: true,
         },
       ];
     }
@@ -65,9 +77,6 @@
     if (nodes) {
       mesh = new Mesh(geometry, material);
       nodes.Scene.add(mesh);
-
-      mesh2 = new Mesh(geometry, material);
-      nodes.Scene.add(mesh2);
     }
   }
 
@@ -81,59 +90,79 @@
 
       if (poseResults && "poseWorldLandmarks" in poseResults) {
         for (const sk of skeleton) {
-          node_pos = poseResults.poseWorldLandmarks[sk.node_id];
-          child_pos = poseResults.poseWorldLandmarks[sk.child_id];
+          node_pos = new Vector3(0, 0, 0);
+          visibility = 0;
+          for (const node_id of sk.node_ids) {
+            node_pos.x =
+              node_pos.x +
+              poseResults.poseWorldLandmarks[node_id].x / sk.node_ids.length;
+            node_pos.y =
+              node_pos.y +
+              poseResults.poseWorldLandmarks[node_id].y / sk.node_ids.length;
+            node_pos.z =
+              node_pos.z +
+              poseResults.poseWorldLandmarks[node_id].z / sk.node_ids.length;
+            visibility =
+              visibility +
+              poseResults.poseWorldLandmarks[node_id].visibility /
+                sk.node_ids.length;
+          }
+
+          child_pos = new Vector3(0, 0, 0);
+          for (const child_id of sk.child_ids) {
+            child_pos.x =
+              child_pos.x +
+              poseResults.poseWorldLandmarks[child_id].x / sk.child_ids.length;
+            child_pos.y =
+              child_pos.y +
+              poseResults.poseWorldLandmarks[child_id].y / sk.child_ids.length;
+            child_pos.z =
+              child_pos.z +
+              poseResults.poseWorldLandmarks[child_id].z / sk.child_ids.length;
+          }
+
           sk.bone?.getWorldPosition(target);
           lookat_pos = new Vector3(
             target.x - (child_pos.x - node_pos.x),
             target.y - (child_pos.y - node_pos.y),
             target.z - (child_pos.z - node_pos.z)
           );
-          if (node_pos.visibility > 0.3) {
+          console.log(lookat_pos, visibility, sk.bone);
+          if (visibility > 0.3) {
+            const old_rot = sk.bone?.rotation.y;
             sk.bone?.lookAt(lookat_pos.x, lookat_pos.y, lookat_pos.z);
+            if (old_rot && sk.bone && sk.excludeXrotation) {
+              sk.bone.rotation.z = old_rot;
+            }
             sk.bone?.rotateX(3.14 / 2);
           }
         }
 
+        const left = poseResults.poseWorldLandmarks[7];
+        const right = poseResults.poseWorldLandmarks[8];
+
+        const leftRight = new Vector3(
+          left.x - right.x,
+          left.y - right.y,
+          left.z - right.z
+        ).normalize();
+        const topDown = new Vector3(0, 1, 0);
+
+        const look = new Vector3(
+          leftRight.y * topDown.z - leftRight.z * topDown.y,
+          -leftRight.x * topDown.z - leftRight.z * topDown.x,
+          leftRight.x * topDown.y - leftRight.y * topDown.x
+        );
+
+        head?.getWorldPosition(target);
+
         if (false) {
-          //head?.lookAt(new Vector3(0.5,2,0.5))
-          mesh.position.x = 0.5;
-          mesh.position.y = 2;
-          mesh.position.z = 0.5;
-
-          const ley = poseResults.poseWorldLandmarks[6];
-          const rey = poseResults.poseWorldLandmarks[3];
-          const lmo = poseResults.poseWorldLandmarks[10];
-          const rmo = poseResults.poseWorldLandmarks[9];
-
-          const diag1 = new Vector3(
-            ley.x - rey.x,
-            ley.y - rey.y,
-            ley.z - rey.z
-          );
-          const diag2 = new Vector3(
-            rey.x + ley.x - rmo.x - lmo.x,
-            rey.y + ley.y - rmo.y - lmo.y,
-            rey.z + ley.z - rmo.z - lmo.z
-          );
-
-          const look = new Vector3(
-            diag1.y * diag2.z - diag1.z * diag2.y,
-            -diag1.x * diag2.z - diag1.z * diag2.x,
-            diag1.x * diag2.y - diag1.y * diag2.x
-          );
-          console.log("llll", diag2, diag1);
-          head?.lookAt(new Vector3(-100 * look.x, -100 * look.y, 100 * look.z));
-
-          head?.getWorldPosition(target);
-          head?.rotateX(-3.14 / 2);
-
-          mesh2.position.x = target.x - 100 * look.x; // left
-          mesh2.position.y = target.y - 100 * look.y; // up
-          mesh2.position.z = target.z + 100 * look.z;
-
-          console.log(mesh2);
+          mesh.position.x = target.x + look.x;
+          mesh.position.y = target.y + look.y;
+          mesh.position.z = target.z + look.z;
         }
+
+        head?.lookAt(target.x + look.x, target.y + look.y, target.z + look.z);
       }
     } else {
       console.log("mo change");
@@ -188,4 +217,4 @@
   });
 </script>
 
-<GLTF castShadow bind:nodes url="assets/brute.glb" />
+<GLTF castShadow bind:nodes url="assets/diablo.glb" />
